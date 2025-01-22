1a94b46e40ffc6962ae06ae507e173e73b00abd3
Franco Canzani <francocanzani@gmail.com>
2025-01-22 22:56:34 +0100

    updates metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,42 +6,49 @@ import SideMenu from '@/components/side-menu';
 const cinzel = Cinzel_Decorative({ subsets: ['latin'], weight: ['400'] });
 
 export const metadata: Metadata = {
-  title: 'Grupo Allora - Authentic Argentine and Lebanese Cuisine',
+  title: 'Grupo Allora - Cocina Argentina y Libanesa Auténtica',
   description:
-    'Experience the rich flavors of Argentina and Lebanon at Grupo Allora. Enjoy gourmet empanadas, Argentine grills, and authentic Lebanese dishes in a vibrant setting.',
+    'Disfruta de los sabores auténticos de Argentina y Líbano en Grupo Allora. Empanadas gourmet, parrillas argentinas y platos libaneses tradicionales en un ambiente vibrante.',
   keywords: [
     'Grupo Allora',
-    'Argentine cuisine',
-    'Lebanese cuisine',
-    'empanadas',
-    'gourmet grills',
-    'Denia restaurants',
+    'Allora Denia',
+    'Patagonia Denia',
+    'Allora Magazinos',
+    'comida argentina',
+    'comida argentina en Denia',
+    'comida argentina en Alicante',
+    'comida libanesa',
+    'comida libanesa en Denia',
+    'empanadas argentinas',
+    'parrillas gourmet',
+    'restaurantes en Denia',
     'Els Magazinos',
+    'Els Magazinos Denia',
   ],
   authors: [{ name: 'Grupo Allora', url: 'https://grupoallora.com' }],
   openGraph: {
-    title: 'Grupo Allora - Authentic Argentine and Lebanese Cuisine',
+    title: 'Grupo Allora - Cocina Argentina y Libanesa Auténtica',
     description:
-      'Experience the rich flavors of Argentina and Lebanon at Grupo Allora. Enjoy gourmet empanadas, Argentine grills, and authentic Lebanese dishes in a vibrant setting.',
+      'Disfruta de los sabores auténticos de Argentina y Líbano en Grupo Allora. Empanadas gourmet, parrillas argentinas y platos libaneses tradicionales en un ambiente vibrante.',
     url: 'https://grupoallora.com',
     siteName: 'Grupo Allora',
     images: [
       {
-        url: 'https://grupoallora.com/og-image.jpg', // Replace with your actual OG image URL
+        url: 'https://grupoallora.com/og-image.jpg', // Reemplaza con la URL de tu imagen OG
         width: 1200,
         height: 630,
-        alt: 'Grupo Allora - Authentic Argentine and Lebanese Cuisine',
+        alt: 'Grupo Allora - Cocina Argentina y Libanesa Auténtica',
       },
     ],
-    locale: 'en_US',
+    locale: 'es_ES',
     type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Grupo Allora - Authentic Argentine and Lebanese Cuisine',
+    title: 'Grupo Allora - Cocina Argentina y Libanesa Auténtica',
     description:
-      'Experience the rich flavors of Argentina and Lebanon at Grupo Allora. Enjoy gourmet empanadas, Argentine grills, and authentic Lebanese dishes in a vibrant setting.',
-    // images: ['https://grupoallora.com/twitter-image.jpg'], // Replace with your actual Twitter image URL
+      'Disfruta de los sabores auténticos de Argentina y Líbano en Grupo Allora. Empanadas gourmet, parrillas argentinas y platos libaneses tradicionales en un ambiente vibrante.',
+    // images: ['https://grupoallora.com/twitter-image.jpg'],
   },
   robots: {
     index: true,
@@ -56,6 +63,9 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
@@ -64,7 +74,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='es'>
+      {' '}
+      {/* Cambiado a español */}
       <body className={`${cinzel.className} antialiased w-full p-3`}>
         <main className='flex w-full items-start justify-between gap-3 flex-col lg:flex-row'>
           <SideMenu />
